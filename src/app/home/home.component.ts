3be8b3705921f1f61c79dfa3773a3c8e0036741e
BlackNeo <webscribo@gmail.com>
2017-11-16 21:39:50 +0100

    Commit message : Fix Login path (test 2)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  private urlPrefix = '';
+   urlPrefix = '';
 
   constructor() { }
 
