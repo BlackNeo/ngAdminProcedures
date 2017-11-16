9c09a1cfbb3e62c6b792111984ca82fcad8196ea
BlackNeo <webscribo@gmail.com>
2017-11-16 20:35:10 +0100

    Commit message : Add routes Home, Users & Login. Bootstrap templates integration.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UsersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
