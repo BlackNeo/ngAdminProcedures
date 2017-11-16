28594ef112ed52c0d59cf65cc5545a476dff5cb2
BlackNeo <webscribo@gmail.com>
2017-11-16 21:20:41 +0100

    Commit message : Fix Home path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,7 @@ import { UsersComponent } from './users/users.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
+  {path: '', component: HomeComponent},
   {path: 'video', component: UsersComponent},
   {path: 'login', component: LoginComponent}
 ];
