eb10d37f4cbd6c4b7ee25afa5c18c1f4638e1ddb
BlackNeo <webscribo@gmail.com>
2017-11-17 02:34:03 +0100

    Commit message : revover

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Users } from './../users';
 
 @Component({
   selector: 'app-login',
@@ -7,6 +8,12 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent implements OnInit {
+
+  users: Users[] = [
+    {'_id': '00001', 'name': 'zerosum', 'password': '1234'},
+    {'_id': '00002', 'name': 'zerosum1', 'password': '12341'},
+    {'_id': '00003', 'name': 'zerosum2', 'password': '12342'}
+  ];
 
   constructor() { }
 
